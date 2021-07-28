ae1087793afcac3b5140512072b3126cb25b9ec2
Richard2203 <55164319+Richard2203@users.noreply.github.com>
2021-07-27 22:12:33 -0500

    creando el evento update

--- controllers/events.js
+++ controllers/events.js
@@ -50,11 +50,60 @@ const crearEvento = async (req, res = response) => {
 	}
 };
 
-const actualizarEvento = (req, res = response) => {
-	res.json({
-		ok: true,
-		msg: 'actualizarEvento',
-	});
+const actualizarEvento = async (req, res = response) => {
+	// el nombre de la propiedad "id" esta definido en el path
+	const eventoID = req.params.id;
+	const uid = req.uid;
+
+	try {
+		const evento = await Evento.findById(eventoID);
+
+		if (!evento)
+			res.status(404).json({
+				ok: false,
+				msg: 'No existe evento con ese ID',
+			});
+
+		if (evento.user.toString() !== uid)
+			res.status(401).json({
+				ok: false,
+				msg: 'No tiene privilegios para editar este evento',
+			});
+
+		// creando un nuevo evento extrayendo las propieades de red.body
+		// y anexando la propieadad "user"
+		const nuevoEvento = {
+			...req.body,
+			user: uid,
+		};
+
+		console.log(nuevoEvento);
+
+		// findByIdAndUpdate() recibe varios parametros
+		// y retorna el elemento actualizado
+		// - primer elemento: id del objeto "evento" a actualizar
+		// - segundo elemento: un objeto con propieades equivalentes al objeto
+		//   a actualizar
+		// - tercer elemento: un objeto con configuraciones: la propieadad "new"
+		//   "new:true" establece el retorno del objeto actualizado, de omitir
+		//   esta propieadad retornara el objeto viejo
+		const eventoActualizado = await Evento.findByIdAndUpdate(
+			eventoID,
+			nuevoEvento,
+			{ new: true }
+		);
+
+		res.json({
+			ok: true,
+			evento: eventoActualizado,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			ok: false,
+			msg: 'por favor hable con el administrador',
+		});
+	}
 };
 
 const eliminarEvento = (req, res = response) => {
